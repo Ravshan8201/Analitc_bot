745cbb816da79d352f891aa1bfd14ba357a2baf0
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2022-01-27 20:06:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,6 @@ dis.add_handler(MessageHandler(Filters.text, next_func))
 dis.add_handler(MessageHandler(Filters.contact, get_contac))
 dis.add_handler(MessageHandler(Filters.photo, adm))
 dis.add_handler(MessageHandler(Filters.video, adm_v))
-class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self, *args):
-    self.kill_now = True
-
-if __name__ == '__main__':
-  killer = GracefulKiller()
-  while not killer.kill_now:
-    time.sleep(1)
-    print("doing something in a loop ...")
-   
-  print("End of the program. I was killed gracefully :)")
 upd.start_polling(drop_pending_updates=True)
+upd.idle()
